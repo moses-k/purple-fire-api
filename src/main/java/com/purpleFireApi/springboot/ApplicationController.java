50944114533e59598324384d73583303237880f8
mosesk <kipyegonmoses1988@gmail.com>
2022-01-14 14:16:52 +0300

    cleaned api project

--- src/main/java/com/purpleFireApi/springboot/ApplicationController.java
+++ src/main/java/com/purpleFireApi/springboot/ApplicationController.java
@@ -5,8 +5,11 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.purpleFireApi.service.AccountInformationService;
@@ -26,36 +29,34 @@ public class ApplicationController {
 	private AccountInformationService accountInfo;
 	@Autowired
 	private AccountStatementService accountStatements;
-	
-	@RequestMapping("/users")
+	@GetMapping(path = "/users") // Compliant
 	public String user() {
 		return admin.retriveNormalUsers();
 	}
 	
-    @RequestMapping(path="user/{username}")
+    @PostMapping(path="user/{username}")
 	public String userInformation(@PathVariable("username") String username) {
 		return userInfo.retriveUserInformation( username);
 	}
 	
-    @RequestMapping(path="accounts/{userid}")
+    @PostMapping(path="accounts/{userid}")
 	public String accountInformation(@PathVariable("userid") String userid) {
 		return accountInfo.retrieveAccountInfo(userid);
 	}
 
-    @RequestMapping("accounts/statements")
+    @GetMapping(path="accounts/statements")
 	public String accountStatement(@RequestParam("accountId") String accountId ) {
     	//return accountId;
 		return accountStatements.retrieveAccountStatements(accountId);
 	}
    
-    @RequestMapping(value = "/logout")
+    @GetMapping(value = "/logout")
     public String logout(HttpServletRequest request, Model m) {
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
             System.out.println("========= invalidating session");
         }
-//       / m.addAttribute("patient", newStuff());
         return "redirect:/login"; 
     }
 }
